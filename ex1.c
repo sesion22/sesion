e27621602a02427c9e6d37523d21e92c8e1e1e9e
Sesion <sesion2299@gmail.com>
2019-05-03 00:34:12 +0600

    first commit

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*This is a comment*/
+int main(int argc, char *argv[])
+{
+	int dictance = 100;
+
+	//this is also is comment	
+	printf("You're %d milles away", distance);
+
+	return 0;
+}
